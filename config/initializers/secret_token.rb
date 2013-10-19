e748ce5d3716107295d5ae3b9f19c3f80c7bd1e7
Vlad Bokov <razum2um@mail.ru>
2013-10-19 09:28:42 +0700

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ThankYou::Application.config.secret_key_base = '9e7dd54ebbcf3a96c99aa4579ba5173056d133b6fc55363ca5eeaebe1bce7a8e4c85318ea2d8dc474b581de74b7f0504657e4657634c46472b1c05ce4cf6bb09'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ThankYou::Application.config.session_store :cookie_store, key: '_thank_you_session'
